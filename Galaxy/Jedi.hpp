ea0ad67f2d94798976d6b7cb6166b06f68af38b2
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-03-25 20:06:47 +0200

    Update Jedi.hpp

--- Galaxy/Jedi.hpp
+++ Galaxy/Jedi.hpp
@@ -1,7 +1,7 @@
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
 #endif
-#include"Stormtrooper.hpp"
+#include"Stormtrooper.h"
 enum class rankOfJedi { Youngling, Padawan, Service, Corps, Knight, Consular, Guardian, Sentinel, Master, Council_Member, Grand_Master };
 class Jedi {
 private:
